54f832186f510c678665ab0c2b994a853f391188
Will Pimblett <will@fullaf.com>
2015-10-14 12:36:47 +0100

    Remove foreground and background colours in mapping dialogue

--- gpe_utils/csvmappingdialog.py
+++ gpe_utils/csvmappingdialog.py
@@ -1,35 +1,31 @@
 import wx
 
 class CSVMappingDlg(wx.Dialog):
-    
+
     def __init__(self, parent,csvColumns,components,lastMapping,needsTime):
-        super(CSVMappingDlg, self).__init__(parent) 
+        super(CSVMappingDlg, self).__init__(parent)
         self.needsTime=needsTime
         self.panel = wx.Panel(self)
         vbox = wx.BoxSizer(wx.VERTICAL)
-        
+
         self.combos=[]
         self.mapping={}
         self.timeColumn=None
-        
+
 
         if needsTime:
             st=wx.StaticText(self.panel,label="Time column (required)")
-            st.SetBackgroundColour((0,0,0))
-            st.SetForegroundColour((255,255,255))
             vbox.Add(st)
             #  need a time column
             self.timeCombo=wx.ComboBox(self.panel,style=wx.CB_READONLY)
             self.timeCombo.AppendItems(csvColumns)
             vbox.Add(self.timeCombo)
-        
-        
+
+
         sensorCombo={}
         for comp in components:
             if hasattr(comp,"setValue"):
                 st=wx.StaticText(self.panel,label=comp.title())
-                st.SetBackgroundColour((0,0,0))
-                st.SetForegroundColour((255,255,255))
 
                 vbox.Add(st,flag=wx.RIGHT|wx.LEFT|wx.EXPAND,border=5)
                 if hasattr(comp,"getNumAxes"):
@@ -57,7 +53,7 @@ class CSVMappingDlg(wx.Dialog):
                     self.timeCombo.SetStringSelection(col)
                 elif sensorCombo.has_key(component):
                     sensorCombo[component].SetStringSelection(col)
-                
+
         # each sensor can only take one mapping
         hbox1=wx.BoxSizer(wx.HORIZONTAL)
         okButton = wx.Button(self.panel, wx.ID_OK)
@@ -66,11 +62,11 @@ class CSVMappingDlg(wx.Dialog):
         hbox1.Add(okButton)
         hbox1.Add(cancelButton, flag=wx.LEFT, border=5)
         vbox.Add(hbox1,flag=wx.ALIGN_RIGHT|wx.TOP,border=5)
-        
+
         self.panel.SetSizer(vbox)
         self.panel.Layout()
         vbox.Fit(self)
-        
+
     def OnOK(self,event):
         if not self.needsTime or self.timeCombo.GetSelection()!=wx.NOT_FOUND:
             anySelected=False
@@ -78,11 +74,11 @@ class CSVMappingDlg(wx.Dialog):
                 if box.GetSelection()!=wx.NOT_FOUND and len(box.GetStringSelection())>0:
                     anySelected=True
             if not anySelected:
-                wx.MessageBox('You need to select at least one mapping from CSV column to sensor', 'Error', 
+                wx.MessageBox('You need to select at least one mapping from CSV column to sensor', 'Error',
                     wx.OK | wx.ICON_WARNING)
                 return True
         else:
-            wx.MessageBox('You need to select a column with the time in it', 'Error', 
+            wx.MessageBox('You need to select a column with the time in it', 'Error',
                 wx.OK | wx.ICON_WARNING)
             return True
         if self.needsTime:
@@ -92,6 +88,6 @@ class CSVMappingDlg(wx.Dialog):
                 self.mapping[combo.GetStringSelection()]=data
         event.Skip()
         return False
-       
+
     def getAssignments(self):
         return self.mapping
